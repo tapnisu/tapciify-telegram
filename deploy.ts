44815fba8b9f52151fb1a124f466d30e869903dc
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-09-03 18:07:42 +0700

    add webhook to deploy

--- deploy.ts
+++ deploy.ts
@@ -1,19 +1,32 @@
 import { webhookCallback } from "grammy";
 import bot from "./mod.ts";
 
-const handleUpdate = webhookCallback(bot, "std/http");
+const handle = webhookCallback(bot, "std/http");
 
 Deno.serve(async (req) => {
-  if (req.method === "POST") {
-    const url = new URL(req.url);
+  const url = new URL(req.url);
 
-    if (url.pathname.slice(1) === bot.token) {
+  if (req.method == "POST") {
+    if (url.pathname == "/bot") {
       try {
-        return await handleUpdate(req);
+        return await handle(req);
       } catch (err) {
         console.error(err);
+        return new Response("Nope, not working...");
       }
     }
+
+    return new Response("What you're trying to post?");
   }
-  return new Response();
+
+  if (url.pathname == "/webhook") {
+    try {
+      await bot.api.setWebhook(`https://${url.hostname}/bot`);
+      return new Response("Done. Set");
+    } catch (_) {
+      return new Response("Couldn't succeed with installing webhook");
+    }
+  }
+
+  return Response.redirect("https://t.me/tapciify_bot", 302);
 });
