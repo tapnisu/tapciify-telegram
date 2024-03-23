5d568e911720aefe9f3cb932e5569522a5b49e2b
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-03-23 13:03:05 +0700

    Update API

--- lib/api.ts
+++ lib/api.ts
@@ -1,84 +1,53 @@
 export interface AsciiArt {
   asciiArt: string;
   width: number;
-  heigth: number;
-}
-
-export interface ConvertResult {
-  data: AsciiArt[];
-}
-
-export interface AsciiCharacter {
-  character: string;
-  r: number;
-  g: number;
-  b: number;
-  a: number;
-}
-
-export interface RawAsciiArt {
-  characters: AsciiCharacter[];
-  width: number;
-  heigth: number;
+  height: number;
 }
 
 export interface RawConvertResult {
-  data: RawAsciiArt[];
+  data: AsciiArt[];
+}
+
+export interface APIError {
+  status: number;
+  message: string;
 }
 
 export class TapciifyApi {
   baseUrl: string;
 
-  constructor(baseUrl = "https://tapciify-api.shuttleapp.rs/api/v1") {
+  constructor(baseUrl = "https://api.tapciify.ru/api/v1") {
     this.baseUrl = baseUrl;
   }
 
   async convert(
-    file: Blob,
-    width = 0,
-    height = 0,
+    file: File,
+    width: number,
+    height: number,
     asciiString = " .,:;+*?%S#@",
-    fontRatio = 0.36
-  ): Promise<ConvertResult> {
+    fontRatio = 0.36,
+    reverse = false
+  ): Promise<RawConvertResult | APIError> {
     const formData = new FormData();
     formData.append("blob", file, "img");
 
-    const req = await fetch(
-      `${
-        this.baseUrl
-      }/convert?width=${width}&height=${height}&fontRatio=${fontRatio}&asciiString=${encodeURIComponent(
-        asciiString
-      )}`,
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
+    let path = `${this.baseUrl}/convert?asciiString=${encodeURIComponent(
+      asciiString
+    )}&fontRatio=${fontRatio}&reverse=${reverse}`;
 
-    return await req.json();
-  }
+    if (width) path += `&width=${width}`;
+    if (height) path += `&height=${height}`;
 
-  async convertRaw(
-    file: Blob,
-    width = 0,
-    height = 0,
-    asciiString = " .,:;+*?%S#@",
-    fontRatio = 0.36
-  ): Promise<RawConvertResult> {
-    const formData = new FormData();
-    formData.append("blob", file, "img");
+    const req = await fetch(path, {
+      method: "POST",
+      body: formData,
+    });
 
-    const req = await fetch(
-      `${
-        this.baseUrl
-      }/convert/raw?width=${width}&height=${height}&fontRatio=${fontRatio}&asciiString=${encodeURIComponent(
-        asciiString
-      )}`,
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
+    if (!req.ok)
+      return {
+        status: req.status,
+        message: await req.text(),
+      };
 
     return await req.json();
   }
